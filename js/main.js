eca529b9ac63439b5516635a03ede5d3eea06643
Mohammed <q34245464@gmail.com>
2024-11-23 13:15:25 +0200

    Commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const quotes = [
+  "Life is 10% what happens to us and 90% how we react to it.",
+  "Do not watch the clock. Do what it does. Keep going.",
+  "The purpose of our lives is to be happy.",
+  "You only live once, but if you do it right, once is enough.",
+  "Be the change that you wish to see in the world.",
+  "It does not matter how slowly you go as long as you do not stop.",
+];
+
+let
+function showQuote() {
+  let randomIndex;
+  let randomQuote;
+
+  do {
+    randomIndex = Math.floor(Math.random() * quotes.length);
+    randomQuote = quotes[randomIndex];
+  } while (randomQuote === lastQuote);
+
+  document.getElementById("quote").textContent = randomQuote;
+
+  lastQuote = randomQuote;
+}
