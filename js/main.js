8571c4b9c19cd16174b68005c01f0527be16948e
Mohammed <q34245464@gmail.com>
2024-11-23 16:46:56 +0200

    Assignment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const quotes = [
   "Life is 10% what happens to us and 90% how we react to it.",
   "Do not watch the clock. Do what it does. Keep going.",
-  "The purpose of our lives is to be happy.",
   "You only live once, but if you do it right, once is enough.",
   "Be the change that you wish to see in the world.",
   "The only limit to our realization of tomorrow is our doubts of today.",
